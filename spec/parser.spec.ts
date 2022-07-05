1c478e381c8c864ef19618df2f36fbbfc819513b
Serabass <serabass565@gmail.com>
2022-07-05 22:48:00 +0600

    upd

--- spec/parser.spec.ts
+++ spec/parser.spec.ts
@@ -1,7 +1,67 @@
-import parser from '../src/parser';
+import peggy from 'peggy';
+import { grammar } from "../src/parser";
+
+function gen(rule: string) {
+  return peggy.generate(grammar, {
+    allowedStartRules: [rule]
+  });
+}
+
+function parse(src: string, rule: string) {
+  let parser = gen(rule);
+  return parser.parse(src, {
+    startRule: rule
+  });
+}
 
 describe('sum', () => {
-  it('sums two numbers', () => {
-    parser.parse('===\n\n');
+  it('Match Separator', () => {
+    let res = parse(`===\n\n`, 'LRXSeparator');
+    expect(res.type).toBe('SEPARATOR');
+  });
+
+  it('Match Document Title', () => {
+    parse(`Мы эхо\n`, 'LRXDocumentTitle');
+  });
+
+  it('Match Block Header', () => {
+    parse(`[1 куплет]`, 'LRXBlockHeader');
+  });
+
+  it('Match Empty Line', () => {
+    parse(`\n`, 'EmptyLine');
+  });
+
+  it('Match Line Bookmark', () => {
+    parse(`~1+1`, 'LineBookmark');
+  });
+
+  it('Match Line Bookmark Rate', () => {
+    parse(`+1`, 'LineBookmarkRate');
+  });
+
+  it('Match Lyric Line sContent', () => {
+    parse(`Давай покрасим холодильник в чёрный цвет~1+1`, 'LyricsLine');
+  });
+
+  it('Match Chords Line', () => {
+    parse(`C#m        Bm\n`, 'ChordsLine');
+  });
+
+  it('Match Chord', () => {
+    parse(`C#m`, 'Chord');
+    parse(`C#m/B`, 'Chord');
+  });
+
+  it('Match ChordBass', () => {
+    parse(`/B`, 'ChordBass');
+  });
+
+  it('Match Line Report', () => {
+    parse(`===
+
+~1 Content
+
+`, 'LRXReport');
   });
 });
--- src/parser.ts
+++ src/parser.ts
@@ -5,7 +5,6 @@ import peggy from 'peggy';
 let file = path.join(__dirname, 'parser.peggy');
 let grammar = fs.readFileSync(file).toString('utf-8');
 
-let parser = peggy.generate(grammar, {
-  allowedStartRules: ['LRXSeparator']
-});
-export default parser;
+let parser = peggy.generate(grammar);
+
+export {grammar, parser};
